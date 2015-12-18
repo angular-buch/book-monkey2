0c3cd99c6d792dcfe8f8f6a0d08796dfce0a2fcd
Ferdinand Malcher <mail@ferdinand-malcher.de>
2015-12-18 03:28:17 +0100

    Components to directives array @JohannesHoppe - we should have read the docs...

--- sprint1/detailansicht/src/app/book-monkey.ts
+++ sprint1/detailansicht/src/app/book-monkey.ts
@@ -3,7 +3,8 @@ import BookDetails from './components/book-details/book-details';
 
 @Component({
   selector: 'book-monkey',
-  template: `<book-details></book-details> :-(`
+  directives: [BookDetails],
+  template: `<book-details></book-details>`
 })
 export default class BookMonkey {
   constructor() { }
