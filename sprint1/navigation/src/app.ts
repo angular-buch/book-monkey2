8dc15aa5d77b52526d5ed0394e509590180b15aa
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-01-03 16:24:42 +0100

    navigation example: HTTP_PROVIDERS not necessary

--- sprint1/navigation/src/app.ts
+++ sprint1/navigation/src/app.ts
@@ -1,6 +1,5 @@
 import { bootstrap } from 'angular2/platform/browser';
-import { HTTP_PROVIDERS } from 'angular2/http';
 import { ROUTER_PROVIDERS } from 'angular2/router';
 import BookMonkey from './app/book-monkey';
 
-bootstrap(BookMonkey, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
+bootstrap(BookMonkey, [ROUTER_PROVIDERS]);
