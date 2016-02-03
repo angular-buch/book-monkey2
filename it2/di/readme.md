216fd5af70e50813e162aa082c4c43029e59bc35
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-02-03 19:39:46 +0100

    added simple runnable di (service) example

--- it2/di/readme.md
+++
@@ -1,1 +0,0 @@
-## TODO: lauffähiges Beispiel! 
---
+++ it2/di/src/app.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {BookMonkeyApp} from './app/book-monkey';
+import {BookStoreService} from './app/services/book-store-service/book-store-service';
+
+bootstrap(BookMonkeyApp);
