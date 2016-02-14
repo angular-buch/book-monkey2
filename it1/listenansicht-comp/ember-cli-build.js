e02eaf61235170077af895a27201e293aaf63e15
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-02-14 23:39:19 +0100

    added semantic-ui to all examples. note that you have to cd in the -folder (for each example) and run [23:39:19] Local gulp not found in ~/git/Angular2Buch/buch/contents/code/book-monkey2 [23:39:19] Try running: npm install gulp to build the semantic-ui dist folder whcih will be copied by angular-cli within running ng serve / ng build @GregOnNet @fmalcher @JohannesHoppe

--- it1/listenansicht-comp/ember-cli-build.js
+++ it1/listenansicht-comp/ember-cli-build.js
@@ -1,8 +1,18 @@
 /* global require, module */
 
 var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
   var app = new Angular2App(defaults);
-  return app.toTree();
-}
+
+  var myTree = new Funnel('./semantic', {
+    srcDir: 'dist',
+    destDir: 'vendor/semantic-ui/dist'
+  });
+
+  var appTree = app.toTree();
+
+  return mergeTrees([myTree, appTree], { overwrite: true });
+};
