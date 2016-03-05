ed148abdc6704ece8ccd341f02136df74ca4131a
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-03-05 20:44:13 +0100

    semantic-ui in first list view example

--- it1/listenansicht/src/app/components/book-list/book-list.ts
+++ it1/listenansicht/src/app/components/book-list/book-list.ts
@@ -3,13 +3,7 @@ import Book from '../../models/book';
 
 @Component({
   selector: 'book-list',
-  template: `
-    <ul>
-        <li *ngFor="#book of books; #i=index">
-            <a href="#">{{ i+1 }}) {{ book.title }}</a>
-            <small>{{ book.isbn }}</small>
-        </li>
-    </ul>`
+  templateUrl: 'app/components/book-list/book-list.html'
 })
 export default class BookList {
   books: Book[];
@@ -17,9 +11,11 @@ export default class BookList {
   constructor() {
       this.books = [
           new Book('999-9-99999-999-9', 'Angular 2', ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode']),
-          new Book('978-3864901546', 'AngularJS: Eine praktische Einführung', ['Philipp Tarasiewicz', 'Robin Böhm']),
+          new Book('978-3864901546', 'AngularJS', ['Philipp Tarasiewicz', 'Robin Böhm']),
           new Book('978-3898647281', 'JavaScript für Enterprise-Entwickler', ['Oliver Ochs'])
       ];
+      this.books[0].subtitle = 'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen';
+      this.books[1].subtitle = 'Eine praktische Einführung';
   }
 
 }
