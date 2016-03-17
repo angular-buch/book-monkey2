7953185720c620b53fa54d97663b35daa8695258
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-03-17 18:20:23 +0100

    replaced placeholder pictures in it1 -> detail view

--- src/app/book-monkey/iteration-1/book-details/book-details.component.ts
+++ src/app/book-monkey/iteration-1/book-details/book-details.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {Book} from '../domain/book';
+import {Book, Thumbnail} from '../domain/book';
 
 @Component({
   selector: 'book-details',
@@ -19,5 +19,9 @@ export class BookDetailsComponent {
     this.book.published = new Date(2016,4,26);
     this.book.rating = 5;
     this.book.description = 'Dieses Buch vermittelt einen Schnelleinstieg in Angular 2 und führt Sie anhand eines anspruchsvollen Beispielprojekts schrittweise an die Entwicklung mit Googles clientseitigem Webframework heran. Lernen Sie dabei Konzepte und Techniken, mit denen Sie strukturierte und modularisierte SinglePageAnwendungen entwickeln. Darüber hinaus lernen Sie Werkzeuge wie Yo, Gulp, Bower, Karma und Protractor zu verwenden und in Ihren Workflow zu integrieren.';
+    this.book.thumbnails = [
+      new Thumbnail('http://goo.gl/nDi0Fc','Buchcover'),
+      new Thumbnail('https://goo.gl/LnYTQD','Angular 2 Logo')
+    ];
   }
 }
