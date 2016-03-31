f0a11be9e77b654da086a3e596f08cf52c6df969
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-03-31 21:54:48 +0200

    besserer Text für Home-Komponente

--- src/app/book-monkey/iteration-2/navigation/home/home.component.ts
+++ src/app/book-monkey/iteration-2/navigation/home/home.component.ts
@@ -1,7 +1,10 @@
 import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
-  template: '<h1>Home</h1> Meine Home-Seite'
+  template: `<h1>Home</h1>
+  Das ist der BookMonkey. Wir können hier <a [routerLink]="['../Books']">Bücher verwalten</a>.`,
+  directives: [ROUTER_DIRECTIVES]
 })
 export class HomeComponent {
   constructor() { }
