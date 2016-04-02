35589f646e0af7facb4745529d617678c1b012ae
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-04-02 17:00:52 +0200

    Better way, more accordingly to mgechev style guide Currently, the activeClass input is not used as CSS class. This is because the input property are evaluated after the HostBinding. This obviously is a bug: https://github.com/angular/angular/issues/4748

--- src/app/book-monkey/iteration-2/navigation/active-class.directive.ts
+++ src/app/book-monkey/iteration-2/navigation/active-class.directive.ts
@@ -1,21 +1,19 @@
-import {Directive, ElementRef, Input, Host} from 'angular2/core';
+import {Directive, ElementRef, Input, HostBinding} from 'angular2/core';
 import {Router, RouterLink} from 'angular2/router';
 
 
 @Directive({
-  selector: '[activeClass]',
-  host: {
-    '[class.active]': 'isRouteActive'
-  }
+  selector: '[activeClass]'
 })
 
 export class ActiveClassDirective {
 
+  @Input() activeClass: string;
   @Input() routerLink: any;
 
   constructor(private _router: Router){ }
 
-  get isRouteActive(): boolean {
+  @HostBinding('class.active') get isRouteActive() : boolean {
     return this._router.isRouteActive(this._router.generate(this.routerLink));
   }
 
