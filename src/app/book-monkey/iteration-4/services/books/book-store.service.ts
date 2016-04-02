d9a66915a3bfd4bd7099b0af9b0d27b271356d42
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-04-02 15:07:19 +0200

    changed bookmonkey api

--- src/app/book-monkey/iteration-4/services/books/book-store.service.ts
+++ src/app/book-monkey/iteration-4/services/books/book-store.service.ts
@@ -6,34 +6,34 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BookStoreService {
-  private api: string = 'https://book-monkey.firebaseio.com';
+  private api: string = 'http://book-monkey2-api.angular2buch.de/books';
 
   constructor(private http: Http) { }
 
   getAll(): Observable<Book[]> {
     return this.http
-      .get(`${this.api}/books.json`)
+      .get(this.api)
       .map(response => response.json());
   }
 
   getSingle(isbn: string) {
     return this.http
-      .get(`${this.api}/books/${isbn}.json`)
+      .get(`${this.api}/${isbn}`)
       .map(response => response.json());
   }
 
   create(book: Book) {
     return this.http
-      .post(`${this.api}/books.json`, JSON.stringify(book))
+      .post(this.api, JSON.stringify(book))
   }
 
   update(book: Book) {
     return this.http
-      .put(`${this.api}/books/${book.isbn}.json`, JSON.stringify(book))
+      .put(`${this.api}/${book.isbn}`, JSON.stringify(book))
   }
 
   delete(isbn: string) {
     return this.http
-      .delete(`${this.api}/books/${isbn}.json`);
+      .delete(`${this.api}/${isbn}`);
   }
 }
