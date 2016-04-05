130ae3c7fe906847ced43ed17e6515423fcbbca0
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-04-05 18:26:13 +0200

    updated isbn-pipe; damit können wir dem Leser zum Einen in der Detailansicht zeigen, dass man die Pipe einfach ohne zusätzliche Paramter anlegen kann (wenn wir nur die Nummer wollen) und zum Anderen in der Listenansicht mit einerm Objekt was wir der Pipe übergeben (um den die Ausgabe  zu erwirken)

--- src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
+++ src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
@@ -4,11 +4,13 @@ import {Pipe, PipeTransform} from 'angular2/core';
   name: 'isbn'
 })
 export class Isbn implements PipeTransform {
-  transform(value: any): any {
-    if(value.length === 10)
-      return `ISBN-10 : ${value}`;
-    if(value.length === 13)
-      return `ISBN-13 : ${value.substr(0,3)}-${value.substr(3)}`;
-    return null;
+  transform(value: string, args?:[string]): string {
+    if(value.length !== 10 && value.length !== 13) return null;
+    let prefix = ''
+    if (args[0]['prefix']){ // add a prefix
+      prefix = (value.length === 10) ? 'ISBN-10 : ' : 'ISBN-13 : ';
+    };
+    if(value.length === 10) return prefix + value;
+    else return `${prefix}${value.substr(0,3)}-${value.substr(3)}`;
   }
 }
