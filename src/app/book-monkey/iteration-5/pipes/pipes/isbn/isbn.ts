3524459be90874c235d3f62acf3edd0fcb83135d
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-04-05 18:32:52 +0200

    IsbnPipe: Code structure

--- src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
+++ src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
@@ -4,11 +4,15 @@ import {Pipe, PipeTransform} from 'angular2/core';
   name: 'isbn'
 })
 export class IsbnPipe implements PipeTransform {
-  transform(value: any): any {
-    if(value.length === 10)
-      return `ISBN-10 : ${value}`;
-    if(value.length === 13)
-      return `ISBN-13 : ${value.substr(0,3)}-${value.substr(3)}`;
-    return null;
+  transform(value: string): any {
+    if(value.length === 10){
+      return `ISBN-10: ${value}`;
+
+    }else if(value.length === 13){
+      return `ISBN-13: ${value.substr(0,3)}-${value.substr(3)}`;
+
+    }else{
+      return null;
+    }
   }
 }
