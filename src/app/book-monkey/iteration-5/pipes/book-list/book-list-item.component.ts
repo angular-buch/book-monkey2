8182f44c2ca026142e153614deb613881f2c8457
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-04-05 18:28:24 +0200

    Pipe name with 'Pipe' See style mgechev style guide https://github.com/mgechev/angular2-style-guide#pipes

--- src/app/book-monkey/iteration-5/pipes/book-list/book-list-item.component.ts
+++ src/app/book-monkey/iteration-5/pipes/book-list/book-list-item.component.ts
@@ -1,11 +1,11 @@
 import {Component, Input} from 'angular2/core';
 import {Book} from '../../domain/book';
-import {Isbn} from '../pipes/isbn/isbn';
+import {IsbnPipe} from '../pipes/isbn/isbn';
 
 @Component({
   selector: '.book-list-item',
   templateUrl: '/app/book-monkey/iteration-5/pipes/book-list/book-list-item.component.html',
-  pipes: [Isbn]
+  pipes: [IsbnPipe]
 })
 
 export class BookListItem {
--- src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
+++ src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
@@ -3,7 +3,7 @@ import {Pipe, PipeTransform} from 'angular2/core';
 @Pipe({
   name: 'isbn'
 })
-export class Isbn implements PipeTransform {
+export class IsbnPipe implements PipeTransform {
   transform(value: any): any {
     if(value.length === 10)
       return `ISBN-10 : ${value}`;
