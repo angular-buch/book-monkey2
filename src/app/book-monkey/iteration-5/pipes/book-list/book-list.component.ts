bb2c60045c80bc55d70f8865df74542ad567207b
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-04-05 16:44:52 +0200

    reverted book-mock for it5 because we have a backend now ;-)

--- src/app/book-monkey/iteration-5/pipes/book-list/book-list.component.ts
+++ src/app/book-monkey/iteration-5/pipes/book-list/book-list.component.ts
@@ -1,7 +1,7 @@
 import {Component} from 'angular2/core';
 import {RouterLink} from 'angular2/router';
 import {BookListItem} from './book-list-item.component';
-import {Book, Thumbnail} from '../../domain/book';
+import {Book} from '../../domain/book';
 import {BookStoreService} from '../services/books/book-store.service';
 
 @Component({
@@ -16,29 +16,6 @@ export class BookListComponent {
   constructor(private bs: BookStoreService) { }
 
   ngOnInit(){
-    //this.bs.getAll().subscribe(res => this.books = res);
-    this.tempMockUntilRESTworks();
-  }
-
-  tempMockUntilRESTworks(){
-    let book1 = new Book(
-      '9783864903571',
-      'Angular 2',
-      ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode']
-    );
-    book1.subtitle = 'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen';
-    book1.thumbnails = [ new Thumbnail('http://goo.gl/nDi0Fc','Buchcover')];
-
-    let book2 = new Book(
-      '9783864901546',
-      'AngularJS',
-      ['Philipp Tarasiewicz', 'Robin Böhm']
-    )
-    book2.subtitle = 'Eine praktische Einführung';
-    book2.thumbnails = [ new Thumbnail('https://goo.gl/Y5lFVE','Buchcover')];
-
-    this.books = [
-      book1, book2
-    ];
+    this.bs.getAll().subscribe(res => this.books = res);
   }
 }
