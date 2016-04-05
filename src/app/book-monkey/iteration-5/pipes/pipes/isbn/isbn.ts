953675e0c703d2b77a3da389b7cad6256d663657
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-04-05 18:57:35 +0200

    ngIf was at wrong place;

--- src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
+++ src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
@@ -7,7 +7,7 @@ export class IsbnPipe implements PipeTransform {
   transform(value: string, args?:[string]): string {
     if(value.length !== 10 && value.length !== 13) return null;
     let prefix = ''
-    if (args[0]['prefix']){ // add a prefix
+    if (args[0] && args[0]['prefix']){ // add a prefix
       prefix = (value.length === 10) ? 'ISBN-10 : ' : 'ISBN-13 : ';
     };
     if(value.length === 10) return prefix + value;
