7db7721a62061fb70f04ac8b963d8e385ed0ad3c
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-04-05 17:25:08 +0200

    isbn pipe

--- src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
+++ src/app/book-monkey/iteration-5/pipes/pipes/isbn/isbn.ts
@@ -4,7 +4,11 @@ import {Pipe, PipeTransform} from 'angular2/core';
   name: 'isbn'
 })
 export class Isbn implements PipeTransform {
-  transform(value: any, args?: any): any {
+  transform(value: any): any {
+    if(value.length === 10)
+      return `ISBN-10 : ${value}`;
+    if(value.length === 13)
+      return `ISBN-13 : ${value.substr(0,3)}-${value.substr(3)}`;
     return null;
   }
 }
