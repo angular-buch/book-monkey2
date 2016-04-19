4e651bc87711e528d3739735e851bb12f06e8372
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-04-19 20:10:47 +0200

    updated domain (for easier creating example books in the first iterations). added  value for displaying in the ui

--- src/client/app/book-monkey/iteration-1/book-list/book-list/book-list.component.ts
+++ src/client/app/book-monkey/iteration-1/book-list/book-list/book-list.component.ts
@@ -10,25 +10,27 @@ export class BookListComponent {
   books: Book[];
 
   constructor() {
-
-     var book1 = new Book(
-      '9783864903571',
-      'Angular 2',
-      ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode']
-    );
-    book1.rating = 5;
-    book1.subtitle = 'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen';
-    book1.thumbnails = [new Thumbnail('http://goo.gl/nDi0Fc','Buchcover')];
-
-    var book2 = new Book(
-      '9783864901546',
-      'AngularJS',
-      ['Philipp Tarasiewicz', 'Robin Böhm']
-    )
-    book2.rating = 5;
-    book2.subtitle = 'Eine praktische Einführung';
-    book2.thumbnails = [new Thumbnail('https://goo.gl/Y5lFVE','Buchcover')];
-
-    this.books = [book1, book2];
+    this.books = [
+      new Book(
+       '9783864903571',
+       'Angular 2',
+       ['Johannes Hoppe', 'Danny Koppenhagen', 'Ferdinand Malcher', 'Gregor Woiwode'],
+       new Date(2016, 5, 26),
+       'Einstieg in die komponentenbasierte Entwicklung von Web- und Mobile-Anwendungen',
+       5,
+       [new Thumbnail('http://goo.gl/nDi0Fc','Buchcover')],
+       'Dieses Buch vermittelt einen Schnelleinstieg in Angular 2...'
+     ),
+     new Book(
+       '9783864901546',
+       'AngularJS',
+       ['Philipp Tarasiewicz', 'Robin Böhm'],
+       new Date(2014, 5, 29),
+       'Eine praktische Einführung',
+       5,
+       [new Thumbnail('https://goo.gl/Y5lFVE','Buchcover')],
+       'Dieses Buch führt Sie anhand eines zusammenhängenden Beispielprojekts...'
+     )
+    ]
   }
 }
--- src/client/app/book-monkey/iteration-1/domain/book.ts
+++ src/client/app/book-monkey/iteration-1/domain/book.ts
@@ -2,14 +2,14 @@ import {Thumbnail} from './thumbnail';
 export {Thumbnail} from './thumbnail';
 
 export class Book {
-  subtitle: string;
-  rating: number;
-  published: Date;
-  description: string;
-  thumbnails: Thumbnail[];
-
   constructor(
     public isbn: string,
     public title: string,
-    public authors: string[]) { }
+    public authors: string[],
+    public published: Date,
+    public subtitle?: string,
+    public rating?: number,
+    public thumbnails?: Thumbnail[],
+    public description?: string
+  ) { }
 }
