1098e278df3981d4bb20d3c47acfa7d3faf17f31
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-04-25 21:35:49 +0200

    refactored it1 step3 (book-details) @fmalcher zur Info: hier wurde zuvor die falsche Ausgangslage als Basis für das gefakte Routing genommen - an der Stelle gitb es ja schon die verbesserte Listenansicht mit der Komponentenverschachtelung

--- src/client/app/book-monkey/iteration-1/book-details/book-list/book-list.component.ts
+++ src/client/app/book-monkey/iteration-1/book-details/book-list/book-list.component.ts
@@ -1,8 +1,10 @@
 import {Component, Output, EventEmitter} from 'angular2/core';
+import {BookListItem} from '../book-list-item/book-list-item.component';
 import {Book, Thumbnail} from '../domain/book';
 
 @Component({
   selector: 'book-list',
+  directives: [BookListItem],
   moduleId: module.id,
   templateUrl: 'book-list.component.html'
 })
