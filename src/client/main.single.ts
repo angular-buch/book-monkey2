e4a91b6a2e251d9f0a63d812bda301449979849d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-05-04 09:09:54 +0200

    keeping deprecated router for now, removing .single files (which are going to be not required for the mini-monkey), moving files

--- src/client/main.single.ts
+++
@@ -1,8 +0,0 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {ROUTER_PROVIDERS} from '@angular/router';
-
-import {SamplesAppComponent} from './app/';
-
-bootstrap(SamplesAppComponent, [
-  ROUTER_PROVIDERS
-]);
--- src/system-config.ts
+++ src/system-config.ts
@@ -19,7 +19,8 @@ const barrels: string[] = [
   '@angular/common',
   '@angular/compiler',
   '@angular/http',
-  '@angular/router',
+  //'@angular/router',
+  '@angular/router-deprecated',
   '@angular/platform-browser',
   '@angular/platform-browser-dynamic',
 
