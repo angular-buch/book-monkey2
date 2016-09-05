e16a57992c2c7fa80c31053ee1c935191f00b26e
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-09-05 14:23:32 +0200

    `ng init` with angular-cli@a647e51

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/';
+import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    //HomeComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    //routing
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
