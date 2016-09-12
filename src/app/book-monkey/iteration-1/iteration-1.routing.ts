0a66b1c1da9c99fa965b7154dc112c2cfc9bf63f
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-09-12 23:46:37 +0200

    nested router-outlets FOR THE WIN!!! :-D

--- src/app/book-monkey/iteration-1/iteration-1.routing.ts
+++ src/app/book-monkey/iteration-1/iteration-1.routing.ts
@@ -5,11 +5,12 @@ import { AppComponent as AppComponent1 } from './book-list/app.component';
 import { AppComponent as AppComponent2 } from './book-list-refactored/app.component';
 import { AppComponent as AppComponent3 } from './book-details/app.component';
 
-const IT1_ROUTES: Routes = [
+export const IT1_ROUTES: Routes = [
   {
     path: 'iteration-1',
+    component: Iteration1Component,
     children: [
-      { path: '', component: Iteration1Component },
+      { path: '', redirectTo: 'book-list', pathMatch: 'full' },
       { path: 'book-list', component: AppComponent1 },
       { path: 'book-list-refactored', component: AppComponent2 },
       { path: 'book-details', component: AppComponent3 }]
