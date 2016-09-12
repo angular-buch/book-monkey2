6d5e0822411b3dd4a17c54d6cf1ef3cebea6ce7d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-09-12 23:19:00 +0200

    basic routing, but without nested router-outlet

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent, pathMatch: 'full' }
 ];
 
 export const routing = RouterModule.forRoot(routes);
--- src/app/book-monkey/iteration-1/iteration-1.routing.ts
+++ src/app/book-monkey/iteration-1/iteration-1.routing.ts
@@ -1,4 +1,4 @@
-import { RouterModule, Routes }   from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { Iteration1Component } from './iteration-1.component';
 import { AppComponent as AppComponent1 } from './book-list/app.component';
@@ -6,10 +6,14 @@ import { AppComponent as AppComponent2 } from './book-list-refactored/app.compon
 import { AppComponent as AppComponent3 } from './book-details/app.component';
 
 const IT1_ROUTES: Routes = [
+  {
+    path: 'iteration-1',
+    children: [
       { path: '', component: Iteration1Component },
       { path: 'book-list', component: AppComponent1 },
       { path: 'book-list-refactored', component: AppComponent2 },
-      { path: 'book-details', component: AppComponent3 },
+      { path: 'book-details', component: AppComponent3 }]
+  }
 ];
 
 export const iteration1Routing = RouterModule.forChild(IT1_ROUTES);
