3b41262b0add17a6b3b1c36d9e629e16e8dfde34
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-09-13 23:44:54 +0200

    footer link with url to mini-monkey repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  repositoryUrl: string;
+  repoName: string;
+
+  constructor(r: Router) {
+    r.events
+      .filter(e => e instanceof NavigationEnd)
+      .subscribe(e => {
+
+        let url: string = (<any>e).urlAfterRedirects;
+
+        if ((url.match(/\//g) || []).length < 2) {
+          this.repoName = '/';
+        } else {
+          this.repoName = url.replace(/\//g, '-').replace(/^-/, '/');
+        }
+
+        this.repositoryUrl = 'https://github.com/book-monkey2-build' + this.repoName;
+      });
+  }
 }
