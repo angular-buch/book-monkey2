72c7805a1085861b421d10231dc6a36ec8a27bde
Gregor Woiwode <gw@co-it.eu>
2016-10-05 21:35:05 +0200

    (it-5) Uses async-Pipe instead of subscribe-Block

--- src/app/book-monkey/iteration-5/pipes/book-list/book-list.component.ts
+++ src/app/book-monkey/iteration-5/pipes/book-list/book-list.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 import { Book } from '../../shared/book';
 import { BookStoreService } from '../../shared/book-store.service';
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'bm-book-list',
   templateUrl: 'book-list.component.html'
 })
 export class BookListComponent implements OnInit {
-  books: Book[];
+  books: Observable<Book[]>;
 
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.bs.getAll().subscribe(res => this.books = res);
+    this.books = this.bs.getAll();
   }
 }
