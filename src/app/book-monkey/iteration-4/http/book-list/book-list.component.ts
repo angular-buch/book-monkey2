fc3803c18e2518e106a15f6c2ae0c9f72de2ea28
Gregor Woiwode <gw@co-it.eu>
2016-10-05 21:36:07 +0200

    (it-4) Reverts usage of asny-Pipe

--- src/app/book-monkey/iteration-4/http/book-list/book-list.component.ts
+++ src/app/book-monkey/iteration-4/http/book-list/book-list.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
-import { Observable } from "rxjs";
 
 @Component({
   selector: 'bm-book-list',
   templateUrl: 'book-list.component.html'
 })
 export class BookListComponent implements OnInit {
-  books: Observable<Book[]>;
+  books: Book[];
 
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
-    this.books = this.bs.getAll();
+    this.bs.getAll().subscribe(res => this.books = res);
   }
 }
