cf6819c0e7c40ec9f061781cda355638c6e85d5b
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-10-06 19:06:06 +0200

    removes form edit mode from template driven forms

--- src/app/book-monkey/iteration-3/template-driven-forms/book-form/book-form.component.ts
+++ src/app/book-monkey/iteration-3/template-driven-forms/book-form/book-form.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
@@ -13,29 +12,13 @@ export class BookFormComponent implements OnInit {
   isUpdatingBook: boolean = false;
 
   constructor(
-    private bs: BookStoreService,
-    private route: ActivatedRoute,
-    private router: Router
+    private bs: BookStoreService
   ) { }
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      let isbn = params['isbn'];
+  ngOnInit() { }
 
-      if (isbn) {
-        this.isUpdatingBook = true;
-        this.book = this.bs.getSingle(isbn);
-      }
-    });
-  }
-
-
-  submitForm() {
-    if (this.isUpdatingBook) {
-      this.bs.update(this.book);
-      this.router.navigate(['../../books', this.book.isbn], { relativeTo: this.route });
-    } else {
-      this.bs.create(this.book);
-    }
+  submitForm(value: {}) {
+    console.log(value);
+    this.bs.create(this.book);
   }
 }
