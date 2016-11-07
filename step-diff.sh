99e31615a361fde6831985bd6a120c13d07df4de
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-11-07 23:56:18 +0100

    Pimped diff script to generate HTML output

--- step-diff.sh
+++ step-diff.sh
@@ -10,18 +10,22 @@ IT4="$BASEDIR/iteration-4"
 IT5="$BASEDIR/iteration-5"
 IT6="$BASEDIR/iteration-6"
 
-mkdir -p diffs
+rm -fr $OUTDIR
+mkdir $OUTDIR
 
-echo "creating .diff-files with diffs between iterations/steps in folder..."
-diff -r $IT1/book-list $IT1/book-list-refactored       > $OUTDIR/it1-1_it1-2.diff;
-diff -r $IT1/book-list-refactored $IT1/book-details    > $OUTDIR/it1-2_it1-3.diff;
-diff -r $IT1/book-details $IT2/di                      > $OUTDIR/it1-3_it2-1.diff;
-diff -r $IT2/di $IT2/navigation                        > $OUTDIR/it2-1_it2-2.diff;
-diff -r $IT2/navigation $IT3/http                      > $OUTDIR/it2-2_it3-1.diff;
-diff -r $IT3/http $IT4/template-driven-forms           > $OUTDIR/it3-1_it4-1.diff;
-diff -r $IT4/template-driven-forms $IT4/reactive-forms > $OUTDIR/it4-1_it4-2.diff;
-diff -r $IT4/reactive-forms $IT4/custom-validation     > $OUTDIR/it4-2_it4-3.diff;
-diff -r $IT4/custom-validation $IT5/pipes              > $OUTDIR/it4-3_it5-1.diff;
-diff -r $IT5/pipes $IT5/directives                     > $OUTDIR/it5-1_it5-2.diff;
-diff -r $IT5/directives $IT6/modules                   > $OUTDIR/it5-2_it6-1.diff;
+DIFF="diff -u -r --unidirectional-new-file"
+DIFF2HTML="node_modules/diff2html-cli/bin/diff2html -i stdin -o stdout --"
+
+echo "creating .html-files with diffs between iterations/steps in folder..."
+$DIFF $IT1/book-list $IT1/book-list-refactored       | $DIFF2HTML > $OUTDIR/it1-1_it1-2.html;
+$DIFF $IT1/book-list-refactored $IT1/book-details    | $DIFF2HTML > $OUTDIR/it1-2_it1-3.html;
+$DIFF $IT1/book-details $IT2/di                      | $DIFF2HTML > $OUTDIR/it1-3_it2-1.html;
+$DIFF $IT2/di $IT2/navigation                        | $DIFF2HTML > $OUTDIR/it2-1_it2-2.html;
+$DIFF $IT2/navigation $IT3/http                      | $DIFF2HTML > $OUTDIR/it2-2_it3-1.html;
+$DIFF $IT3/http $IT4/template-driven-forms           | $DIFF2HTML > $OUTDIR/it3-1_it4-1.html;
+$DIFF $IT4/template-driven-forms $IT4/reactive-forms | $DIFF2HTML > $OUTDIR/it4-1_it4-2.html;
+$DIFF $IT4/reactive-forms $IT4/custom-validation     | $DIFF2HTML > $OUTDIR/it4-2_it4-3.html;
+$DIFF $IT4/custom-validation $IT5/pipes              | $DIFF2HTML > $OUTDIR/it4-3_it5-1.html;
+$DIFF $IT5/pipes $IT5/directives                     | $DIFF2HTML > $OUTDIR/it5-1_it5-2.html;
+$DIFF $IT5/directives $IT6/modules                   | $DIFF2HTML > $OUTDIR/it5-2_it6-1.html;
 echo "finished job. Diffs stored in folder: $OUTDIR"
