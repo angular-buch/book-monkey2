1720f727b3a13c36a9ae4772389a41ef51e516da
Gregor Woiwode <gw@co-it.eu>
2016-11-09 20:35:27 +0100

    Simplifies assign of publishedDate to form

--- src/app/book-monkey/iteration-6/routing/admin/book-form/book-form.component.ts
+++ src/app/book-monkey/iteration-6/routing/admin/book-form/book-form.component.ts
@@ -47,7 +47,7 @@ export class BookFormComponent implements OnInit {
       description: [this.book.description],
       authors: this.buildAuthorsArray(),
       thumbnails: this.buildThumbnailsArray(),
-      published: [new Date(this.book.published)]
+      published: this.book.published
     });
     this.myForm.valueChanges.subscribe(() => this.updateErrorMessages());
   }
