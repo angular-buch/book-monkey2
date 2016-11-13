8eed7046c19adb88732101f43c7ef4b34272da16
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-11-13 23:12:31 +0100

    + book-store.service.spec.ts (it2)

---
+++ src/app/book-monkey/iteration-2/di/shared/book-store.service.spec.ts
@@ -0,0 +1,15 @@
+import { BookStoreService } from './book-store.service';
+
+describe('BookStoreService', () => {
+
+  let service: BookStoreService;
+
+  beforeEach(() => {
+    service = new BookStoreService();
+  });
+
+  it('should hold a hardcoded list of 2 books', () => {
+    let books  = service.getAll();
+    expect(books.length).toBe(2);
+  });
+});
--- src/app/book-monkey/iteration-5/pipes/shared/isbn-pipe.spec.ts
+++ src/app/book-monkey/iteration-5/pipes/shared/isbn-pipe.spec.ts
@@ -2,22 +2,22 @@ import { IsbnPipe } from './isbn-pipe';
 
 describe('IsbnPipe', () => {
 
-  let isbnPipe = new IsbnPipe();
+  let pipe = new IsbnPipe();
 
   it('should ignore unkown values', () => {
-    expect(isbnPipe.transform('XXX', true)).toBe(null);
+    expect(pipe.transform('XXX', true)).toBe(null);
   });
 
   it('should add a prefix', () => {
-    expect(isbnPipe.transform('3864903572', true)).toMatch(/^ISBN-10: /);
-    expect(isbnPipe.transform('9783864903571', true)).toMatch(/^ISBN-13: /);
+    expect(pipe.transform('3864903572', true)).toMatch(/^ISBN-10: /);
+    expect(pipe.transform('9783864903571', true)).toMatch(/^ISBN-13: /);
   });
 
   it('should keep ISBN-10 unchanged', () => {
-    expect(isbnPipe.transform('3864903572', false)).toBe('3864903572');
+    expect(pipe.transform('3864903572', false)).toBe('3864903572');
   });
 
   it('should format ISBN-13 with a dash', () => {
-    expect(isbnPipe.transform('9783864903571', false)).toBe('978-3864903571');
+    expect(pipe.transform('9783864903571', false)).toBe('978-3864903571');
   });
 });
