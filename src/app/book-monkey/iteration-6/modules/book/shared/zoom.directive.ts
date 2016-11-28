a2e8ff8103215e6cba2332051c0ea58a1170fa88
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-11-28 20:04:51 +0100

    better version of bmZoom-directive
    - uses @HostBinding now
    - removes @Input because it was unused
    - adds snippet for the other version of the directive (with renderer/elementRef)
    
    @GregOnNet @fmalcher @johanneshoppe

--- src/app/book-monkey/iteration-6/modules/book/shared/zoom.directive.ts
+++ src/app/book-monkey/iteration-6/modules/book/shared/zoom.directive.ts
@@ -1,17 +1,13 @@
-import { Directive, Input, ElementRef, HostListener, Renderer } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
-@Directive({
-  selector: '[bmZoom]'
-})
+@Directive({ selector: '[bmZoom]'})
 export class ZoomDirective {
-  @Input() bmZoom: string; // zoom size
-
-  constructor(private el: ElementRef, private renderer: Renderer) { }
+  @HostBinding('class.small') isZoomed: boolean;
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.renderer.setElementClass(this.el.nativeElement, this.bmZoom || 'small', true);
+    this.isZoomed = true;
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.renderer.setElementClass(this.el.nativeElement, this.bmZoom || 'small', false);
+    this.isZoomed = false;
   }
 }
