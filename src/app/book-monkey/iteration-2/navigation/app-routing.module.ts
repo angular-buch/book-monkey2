8687b7b81176e4506ce51c817636f9ca137c5640
Danny Koppenhagen <mail@d-koppenhagen.de>
2016-11-30 18:52:09 +0100

    same style for route definitions

--- src/app/book-monkey/iteration-2/navigation/app-routing.module.ts
+++ src/app/book-monkey/iteration-2/navigation/app-routing.module.ts
@@ -6,10 +6,23 @@ import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'books', component: BookListComponent },
-  { path: 'books/:isbn', component: BookDetailsComponent }
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'books',
+    component: BookListComponent
+  },
+  {
+    path: 'books/:isbn',
+    component: BookDetailsComponent
+  }
 ];
 
 @NgModule({
