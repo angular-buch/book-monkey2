534761636c845871d32e65ac143490c488dabc05
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-12-12 18:39:38 +0100

    shortening the test a bit

--- src/app/book-monkey/iteration-1/book-details/book-list/book-list.component.shallow.spec.ts
+++ src/app/book-monkey/iteration-1/book-details/book-list/book-list.component.shallow.spec.ts
@@ -1,20 +1,18 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { BookListComponent } from './book-list.component';
 import { Book } from '../shared/book';
 
-describe('BookListComponent', () => {
+fdescribe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BookListComponent],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
+        declarations: [BookListComponent],
+        schemas: [NO_ERRORS_SCHEMA]
+      })
       .compileComponents();
   }));
 
@@ -30,8 +28,6 @@ describe('BookListComponent', () => {
     component.showDetailsEvent.subscribe((book) => receivedBook = book);
 
     fixture.nativeElement.querySelector('a').click();
-    fixture.detectChanges();
-
     expect(receivedBook.title).toBe('Angular');
   });
 });
