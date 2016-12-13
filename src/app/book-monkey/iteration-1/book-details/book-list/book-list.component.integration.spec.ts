ae2ccbaa1b28c8bcfa3b8e344d37d8f1c72eb5b4
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-12-13 19:13:57 +0100

    intergration-tests

--- src/app/book-monkey/iteration-1/book-details/book-list/book-list.component.integration.spec.ts
+++ src/app/book-monkey/iteration-1/book-details/book-list/book-list.component.integration.spec.ts
@@ -2,7 +2,10 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListComponent } from './book-list.component';
+import { BookListItemComponent } from '../book-list-item/book-list-item.component';
 import { Book } from '../shared/book';
+
+// import { AppModule } from '../app.module';
 
 describe('BookListComponent', () => {
   let component: BookListComponent;
@@ -10,8 +13,10 @@ describe('BookListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-        declarations: [BookListComponent],
-        schemas: [NO_ERRORS_SCHEMA]
+        declarations: [BookListComponent, BookListItemComponent],
+
+        // also possible, importing the complete module
+        // imports: [AppModule]
       })
       .compileComponents();
   }));
@@ -28,6 +33,16 @@ describe('BookListComponent', () => {
     component.showDetailsEvent.subscribe((book) => receivedBook = book);
 
     fixture.nativeElement.querySelector('a').click();
+    expect(receivedBook.title).toBe('Angular');
+  });
+
+
+  it('should emit the showDetailsEvent when clicking the image', () => {
+
+    let receivedBook: Book;
+    component.showDetailsEvent.subscribe((book) => receivedBook = book);
+
+    fixture.nativeElement.querySelector('img').click();
     expect(receivedBook.title).toBe('Angular');
   });
 });
