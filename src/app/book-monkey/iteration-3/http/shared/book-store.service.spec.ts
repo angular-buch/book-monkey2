0413fd7665249ebe889c220ba8876dc66dabc10b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-12-14 23:50:13 +0100

    removes asyc() from http test

--- src/app/book-monkey/iteration-3/http/shared/book-store.service.spec.ts
+++ src/app/book-monkey/iteration-3/http/shared/book-store.service.spec.ts
@@ -39,55 +39,53 @@ describe('BookStoreService', () => {
   });
 
   it('should GET a list of all books from /books',
-    async(inject([BookStoreService, MockBackend], (service: BookStoreService, backend: MockBackend) => {
+    inject([BookStoreService, MockBackend], (service: BookStoreService, backend: MockBackend) => {
 
-      backend.connections.subscribe((c: MockConnection) => {
-
-        expect(c.request.method).toBe(RequestMethod.Get);
-        expect(c.request.url).toBe('https://book-monkey2-api.angular-buch.com/books');
-
+      let connection: MockConnection, receivedBooks: Book[];
+      backend.connections.subscribe(c => {
+        connection = c;
         c.mockRespond(new Response(new ResponseOptions({
           body: JSON.stringify(expectedBooks)
         })));
       });
 
-      service.getAll().subscribe((books) => {
-        expect(books.length).toBe(2);
-        expect(books[0].isbn).toBe('111');
-        expect(books[1].isbn).toBe('222');
-      });
-    })));
+      service.getAll().subscribe(b => receivedBooks = b);
+
+      expect(connection.request.method).toBe(RequestMethod.Get);
+      expect(connection.request.url).toBe('https://book-monkey2-api.angular-buch.com/books');
+      expect(receivedBooks.length).toBe(2);
+      expect(receivedBooks[0].isbn).toBe('111');
+      expect(receivedBooks[1].isbn).toBe('222');
+    }));
 
   it('should GET a single book from /book/:isbn',
-    async(inject([BookStoreService, MockBackend], (service: BookStoreService, backend: MockBackend) => {
+    inject([BookStoreService, MockBackend], (service: BookStoreService, backend: MockBackend) => {
 
-      backend.connections.subscribe((c: MockConnection) => {
-
-        expect(c.request.method).toBe(RequestMethod.Get);
-        expect(c.request.url).toBe('https://book-monkey2-api.angular-buch.com/book/111');
-
+      let connection: MockConnection, receivedBook: Book;
+      backend.connections.subscribe(c => {
+        connection = c;
         c.mockRespond(new Response(new ResponseOptions({
           body: JSON.stringify(expectedBooks[0])
         })));
       });
 
-      service.getSingle('111').subscribe((book) => {
-        expect(book.isbn).toBe('111');
-      });
-    })));
+      service.getSingle('111').subscribe(b => receivedBook = b);
+
+      expect(connection.request.method).toBe(RequestMethod.Get);
+      expect(connection.request.url).toBe('https://book-monkey2-api.angular-buch.com/book/111');
+      expect(receivedBook.isbn).toBe('111');
+    }));
 
   it('should POST a new book to /book',
-    async(inject([BookStoreService, MockBackend], (service: BookStoreService, backend: MockBackend) => {
+    inject([BookStoreService, MockBackend], (service: BookStoreService, backend: MockBackend) => {
 
-      backend.connections.subscribe((c: MockConnection) => {
-
-        expect(c.request.method).toBe(RequestMethod.Post);
-        expect(c.request.url).toBe('https://book-monkey2-api.angular-buch.com/book');
-        expect(c.request.getBody()).toEqual(JSON.stringify(expectedBooks[0]));
-
-        c.mockRespond(new Response(new ResponseOptions({})));
-      });
+      let connection: MockConnection;
+      backend.connections.subscribe(c => connection = c);
 
       service.create(expectedBooks[0]).subscribe();
-    })));
+
+      expect(connection.request.method).toBe(RequestMethod.Post);
+      expect(connection.request.url).toBe('https://book-monkey2-api.angular-buch.com/book');
+      expect(connection.request.getBody()).toEqual(JSON.stringify(expectedBooks[0]));
+    }));
 });
