e4e9f07920c998026f3df6b406f06b7fc383b178
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-12-27 15:30:11 +0100

    typo in Spec

--- src/app/book-monkey/iteration-5/pipes/shared/isbn-pipe.spec.ts
+++ src/app/book-monkey/iteration-5/pipes/shared/isbn-pipe.spec.ts
@@ -4,7 +4,7 @@ describe('IsbnPipe', () => {
 
   let pipe = new IsbnPipe();
 
-  it('should ignore unkown values', () => {
+  it('should ignore unknown values', () => {
     expect(pipe.transform('XXX', true)).toBe(null);
   });
 
@@ -13,7 +13,7 @@ describe('IsbnPipe', () => {
     expect(pipe.transform('9783864903571', true)).toMatch(/^ISBN-13: /);
   });
 
-  it('should keep ISBN-10 unchanged', () => {
+  it('should not change the format of ISBN-10', () => {
     expect(pipe.transform('3864903572', false)).toBe('3864903572');
   });
 
