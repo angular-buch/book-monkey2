d6a25c43853e7f22a300589351a58c66f8d68493
Danny Koppenhagen <mail@d-koppenhagen.de>
2017-01-08 12:48:47 +0100

    fixes pathes

--- src/app/book-monkey/iteration-1/iteration-1-routing.module.ts
+++ src/app/book-monkey/iteration-1/iteration-1-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { Iteration1Component } from './iteration-1.component';
-import { AppComponent as AppComponentBookList } from './book-list/app.component';
-import { AppComponent as AppComponentBookListRefactored } from './book-list-refactored/app.component';
-import { AppComponent as AppComponentBookDetails } from './book-details/app.component';
+import { AppComponent as AppComponentBookList } from './components/app.component';
+import { AppComponent as AppComponentBookListRefactored } from './property-bindings/app.component';
+import { AppComponent as AppComponentBookDetails } from './event-bindings/app.component';
 
 export const routes: Routes = [
   {
--- src/app/book-monkey/iteration-1/iteration-1.module.ts
+++ src/app/book-monkey/iteration-1/iteration-1.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 
-import { AppModule as AppModuleBookList } from './components/app.module';
-import { AppModule as AppModuleBookListRefactored } from './property-bindings/app.module';
-import { AppModule as AppModuleBookDetails } from './event-bindings/app.module';
+import { AppModule as AppModuleComponent } from './components/app.module';
+import { AppModule as AppModulePropertyBindings } from './property-bindings/app.module';
+import { AppModule as AppModuleEventBindings } from './event-bindings/app.module';
 
 import { Iteration1Component } from './iteration-1.component';
 import { Iteration1RoutingModule } from './iteration-1-routing.module';
@@ -10,9 +10,9 @@ import { Iteration1RoutingModule } from './iteration-1-routing.module';
 @NgModule({
   declarations: [Iteration1Component],
   imports: [
-    AppModuleBookList,
-    AppModuleBookListRefactored,
-    AppModuleBookDetails,
+    AppModuleComponent,
+    AppModulePropertyBindings,
+    AppModuleEventBindings,
     Iteration1RoutingModule
   ]
 })
