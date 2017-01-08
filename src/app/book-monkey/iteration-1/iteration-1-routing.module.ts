31754146ca026d223a731032e062d6d30afe52b8
Danny Koppenhagen <mail@d-koppenhagen.de>
2017-01-08 12:51:19 +0100

    consistent components names

--- src/app/book-monkey/iteration-1/iteration-1-routing.module.ts
+++ src/app/book-monkey/iteration-1/iteration-1-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { Iteration1Component } from './iteration-1.component';
-import { AppComponent as AppComponentBookList } from './components/app.component';
-import { AppComponent as AppComponentBookListRefactored } from './property-bindings/app.component';
-import { AppComponent as AppComponentBookDetails } from './event-bindings/app.component';
+import { AppComponent as AppComponentComponents } from './components/app.component';
+import { AppComponent as AppComponentPropertyBindings } from './property-bindings/app.component';
+import { AppComponent as AppComponentEventBindings } from './event-bindings/app.component';
 
 export const routes: Routes = [
   {
@@ -12,9 +12,9 @@ export const routes: Routes = [
     component: Iteration1Component,
     children: [
       { path: '', redirectTo: 'components', pathMatch: 'full' },
-      { path: 'components', component: AppComponentBookList },
-      { path: 'property-bindings', component: AppComponentBookListRefactored },
-      { path: 'event-bindings', component: AppComponentBookDetails }]
+      { path: 'components', component: AppComponentComponents },
+      { path: 'property-bindings', component: AppComponentPropertyBindings },
+      { path: 'event-bindings', component: AppComponentEventBindings }]
   }
 ];
 
