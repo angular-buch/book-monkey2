6ea713da2bc50acbddd5f06982d2551b63f50850
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-01-18 21:16:43 +0100

    indentation

--- e2e/dpunkt.e2e-spec.ts
+++ e2e/dpunkt.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { browser, element, by } from 'protractor';
 
-describe('dpunkt.verlag', function() {
+describe('dpunkt.verlag', function () {
 
-    // does not wait for on angular on a non-angular page
-    beforeEach(() => browser.ignoreSynchronization = true);
+  // does not wait for on angular on a non-angular page
+  beforeEach(() => browser.ignoreSynchronization = true);
 
-    it('should just call it Angular', function() {
-        browser.get('http://dpunkt.de/buecher/12400/9783864903571-angular.html');
-        let heading = element(by.tagName('h3'));
-        expect(heading.getText()).toEqual('Angular');
-    });
+  it('should just call it Angular', function () {
+    browser.get('http://dpunkt.de/buecher/12400/9783864903571-angular.html');
+    let heading = element(by.tagName('h3'));
+    expect(heading.getText()).toEqual('Angular');
+  });
 });
 
