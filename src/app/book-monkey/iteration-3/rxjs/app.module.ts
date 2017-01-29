2cc64e7a1ea1542dea19ea2d51951df013fb78ac
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-01-30 00:40:28 +0100

    ui loading search

--- src/app/book-monkey/iteration-3/rxjs/app.module.ts
+++ src/app/book-monkey/iteration-3/rxjs/app.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http'; // NEW
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { SearchComponent } from './home/search.component';
 
 import { BookStoreService } from './shared/book-store.service';
 import { AppRoutingModule } from './app-routing.module.one-app';
@@ -18,7 +19,8 @@ import { AppRoutingModule } from './app-routing.module.one-app';
     HomeComponent,
     BookListComponent,
     BookListItemComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/book-monkey/iteration-3/rxjs/home/search.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bm-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent { }
