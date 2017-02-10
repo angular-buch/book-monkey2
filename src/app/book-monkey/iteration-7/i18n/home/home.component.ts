d2b54e4cb7a382a7566ae5d2d584ff21e2591fe9
Johannes Hoppe <Johannes.Hoppe.ext@heidelberg.com>
2017-02-10 17:42:10 +0100

    full messages.xmb for the BM

--- src/app/book-monkey/iteration-7/i18n/home/home.component.ts
+++ src/app/book-monkey/iteration-7/i18n/home/home.component.ts
@@ -9,9 +9,9 @@ import { Book } from '../shared/book';
   <div class="ui container two column grid">
 
     <div class="ui container column">
-      <h1>Home</h1>
-      <p>Das ist der BookMonkey.</p>
-      <a routerLink="../books" class="ui red button">
+      <h1 i18n="HomeComponent:header|">Home</h1>
+      <p i18n="HomeComponent:tagline|a proud sentence about the project">Das ist der BookMonkey.</p>
+      <a routerLink="../books" class="ui red button" i18n="HomeComponent:book list link|Text of the link to the books screen">
         Buchliste ansehen
         <i class="right arrow icon"></i>
       </a>
