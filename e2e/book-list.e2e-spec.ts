5cadd647047d58ee7419f35791b6b79342a572c1
Danny Koppenhagen <mail@d-koppenhagen.de>
2017-02-26 18:28:29 +0100

    BM:e2e - less empty lines; removes LaTeX-Highlights from Master branch

--- e2e/book-list.e2e-spec.ts
+++ e2e/book-list.e2e-spec.ts
@@ -4,22 +4,19 @@
 import { BookListPage } from './book-list.po';
 
 describe('Book List Page', function() {
-
   let listPage: BookListPage;
 
 <TEX>\bfseries </TEX>  beforeEach(() => listPage = new BookListPage());
 
   it('should display at least two books', () => {
-
 <TEX>\boldline</TEX>    const bookItems = listPage.navigateTo().getBookItems();
     expect(bookItems.count()).toBeGreaterThan(1);
   });
 
   it('should navigate to details page by ISBN', () => {
-
-<TEX>\boldline</TEX>    const details = listPage.navigateTo().clickOnFirstBook();
-
-    expect(details.getUrl()).toContain('/books/9783864903571');
-    expect(details.getHeaderText()).toBe('Angular');
+<TEX>\boldline</TEX>    const detailsPage = listPage.navigateTo().clickOnFirstBook();
+    expect(detailsPage.getUrl()).toContain('/books/9783864903571');
+    expect(detailsPage.getHeaderText()).toBe('Angular');
+    const details = listPage.navigateTo().clickOnFirstBook();
   });
 });
