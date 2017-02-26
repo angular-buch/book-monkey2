c6ff3d1537abec339bf4ccf79078ab6183562e7a
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-02-26 13:28:58 +0100

    Updated guard and guard snippets

--- src/app/book-monkey/iteration-6/guards/can-navigate-to-admin.guard.ts
+++ src/app/book-monkey/iteration-6/guards/can-navigate-to-admin.guard.ts
@@ -6,7 +6,7 @@ export class CanNavigateToAdminGuard implements CanActivate {
 
 <TEX>\boldline</TEX>  accessGranted = false;
 
-  canActivate() {
+  canActivate(): boolean {
     if (!this.accessGranted) {
       this.accessGranted = window.confirm('Mit großer Macht kommt große Verantwortung. Möchten Sie den Admin-Bereich betreten?');
     }
